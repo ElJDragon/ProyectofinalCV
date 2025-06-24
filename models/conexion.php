626db840a58734ae0b79289b7b4f63cf19c07e2c
Josue Guevara <el.jdragon@gmail.com>
2025-06-23 20:28:35 -0500

    cambios para el hosteo d ela BD

--- models/conexion.php
+++ models/conexion.php
@@ -1,15 +1,16 @@
 <?php
 class Conexion {
     public function conectar() {
-        $servename = "sql111.infinityfree.com"; // host proporcionado
-        $username  = "if0_39291915";            // tu usuario MySQL
-        $password  = "EEvMHnVnLwnlCbz";         // tu contraseña MySQL
-        $database  = "if0_39291915_proyectocv"; // tu base de datos creada
+        $host = getenv("DB_HOST");
+        $port = getenv("DB_PORT");
+        $dbname = getenv("DB_NAME");
+        $user = getenv("DB_USER");
+        $password = getenv("DB_PASSWORD");
 
-        $conn = mysqli_connect($servename, $username, $password, $database);
+        $conn = new mysqli($host, $user, $password, $dbname, (int)$port);
 
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
         }
 
         return $conn;
